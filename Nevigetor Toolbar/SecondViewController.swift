c381d7eb943a73d0aeefc591449e5ffa335d5324
Student11 <zado05062011@gmail.com>
2017-06-14 16:45:38 +0700

    Create SecondViewController OK

---
+++ Nevigetor Toolbar/SecondViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SecondViewController.swift
+//  Nevigetor Toolbar
+//
+//  Created by Student11 on 6/14/2560 BE.
+//  Copyright © 2560 SNRU. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print("This SecondViwecontroller")
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Nevigetor Toolbar/ViewController.swift
+++ Nevigetor Toolbar/ViewController.swift
@@ -9,10 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //implicit
+    var intnumber = 1
+    
+    
+    
+    
+    @IBAction func increaseNumber(_ sender: Any) {
+        showMessage(strMessage: "Clivk increaseNumber")
+        intnumber += 1
+        if intnumber >= 10 {
+            intnumber = -10
+        }
+        shownumber(intnumber: intnumber)
+    
+    }
+    
+    @IBAction func decreaseNumber(_ sender: Any) {
+        showMessage(strMessage: "Clivk DecreaseNumber")
+        intnumber -= 1
+        shownumber(intnumber: intnumber)
+    }
+    
+    @IBAction func ResetNumber(_ sender: Any) {
+        showMessage(strMessage: "Clivk ResetNumber")
+        intnumber = 1
+        shownumber(intnumber: intnumber)
+    }
+    
+    @IBAction func ReplyNumber(_ sender: Any) {
+        showMessage(strMessage: "Clivk RePlyNumber")
+    }
+    
+    @IBAction func TrashNumber(_ sender: Any) {
+        showMessage(strMessage: "Clivk TrashNumber")
+    }
+    
+    @IBOutlet weak var NumberLabel: UILabel!
+    
+    
+    
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    } //Main Method
+    
+    
+    func showMessage(strMessage: String) -> Void {
+        print("Message ==> \(strMessage)")
+    }
+
+    
+    func shownumber(intnumber: Int) -> Void {
+        let strnumber = String(intnumber)
+        NumberLabel.text = strnumber
+        
     }
 
     override func didReceiveMemoryWarning() {
